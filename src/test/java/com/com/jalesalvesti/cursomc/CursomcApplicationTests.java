ed33f1f41cb1ea2ca2e00dda906f13168d8c116f
jales.alvesTI <jales.alvesti@gmail.com>
2021-05-31 14:10:47 -0300

    Projeto Criado

---
+++ src/test/java/com/com/jalesalvesti/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.jalesalvesti.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
