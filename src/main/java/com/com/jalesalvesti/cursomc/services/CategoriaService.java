d74ff771be70636089b0cf528d376bb30747c566
jales.alvesTI <jales.alvesti@gmail.com>
2021-06-03 17:07:41 -0300

    Deletando uma categoria com DELETE, criação da execption de registro exclusivo

--- src/main/java/com/com/jalesalvesti/cursomc/services/CategoriaService.java
+++ src/main/java/com/com/jalesalvesti/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.com.jalesalvesti.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.com.jalesalvesti.cursomc.domain.Categoria;
 import com.com.jalesalvesti.cursomc.repositories.CategoriaRepository;
+import com.com.jalesalvesti.cursomc.services.exceptions.DataIntegrityException;
 import com.com.jalesalvesti.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());		
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e ){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 }
