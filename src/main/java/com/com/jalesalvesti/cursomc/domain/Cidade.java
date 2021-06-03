6ad70f4d04f1eccf1148497d49ba9ce623850826
jales.alvesTI <jales.alvesti@gmail.com>
2021-06-03 15:58:27 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/com/jalesalvesti/cursomc/domain/Cidade.java
+++ src/main/java/com/com/jalesalvesti/cursomc/domain/Cidade.java
@@ -1,14 +1,13 @@
 package com.com.jalesalvesti.cursomc.domain;
 
 import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cidade implements Serializable {
@@ -20,7 +19,6 @@ public class Cidade implements Serializable {
 	
 	private String nome;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
