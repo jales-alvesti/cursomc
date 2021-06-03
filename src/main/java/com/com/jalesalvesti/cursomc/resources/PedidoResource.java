15189eff63f30fb875bd407c3725d5e5c44e0318
jales.alvesTI <jales.alvesti@gmail.com>
2021-06-03 15:46:56 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/com/jalesalvesti/cursomc/resources/PedidoResource.java
@@ -0,0 +1,25 @@
+package com.com.jalesalvesti.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.com.jalesalvesti.cursomc.domain.Pedido;
+import com.com.jalesalvesti.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)	
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = service.Buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
